2e77fc2d2cc0069695d12aa5caf9f8c07c75a6a1
vascoalexander <vascoalexander@gmail.com>
2024-08-30 15:39:14 +0200

    show results, copy to clipboard

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import Subnetz as net
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
 import ttkbootstrap as ttk
+import ipaddress
 
 class App(ttk.Window):
     def __init__(self):
@@ -14,18 +15,61 @@ class App(ttk.Window):
         self.entry_sub = Entry(self, 'XXX', 'readonly', 'Subnetmask')
         self.radio_buttons = Toggle(self, self.entry_sub, self.entry_ip)
 
-        self.button_submit = ttk.Button(self, text='Submit', command=self.get_cidr_value)
+        self.button_submit = ttk.Button(self, text='Submit', command=self.show_network)
         self.button_submit.pack(pady=(10, 10))
 
-        self.result = FrameResult(self).pack(expand=True, fill='x', padx=20)
+        self.result_frame = FrameResult(self)
+        self.result_frame.pack(expand=True, fill='x', padx=20)
 
-        self.entry_ip.cidr_field.bind('<Return>', self.get_cidr_value)
+        self.entry_ip.cidr_field.bind('<Return>', self.update_subnetmask)
 
         self.mainloop()
 
-    def get_cidr_value(self,event=None):
-        cidr_value = self.entry_ip.get_cidr_value()
-        subnetmask = net.cidr_to_sub(cidr_value).split('.')
+    def show_network(self):
+        ip = self.validate_ip()
+        subnetmask = self.get_mask()
+        try:
+            network = ipaddress.ip_network((ip + '/' + subnetmask), strict=False)
+            self.result_frame.update_labels(
+                network_id=network.network_address,
+                first_host=net.first_host_get(network),
+                last_host=net.last_host_get(network),
+                broadcast=network.broadcast_address,
+                max_hosts=network.num_addresses - 2
+            )
+            self.update_subnetmask(subnetmask.split('.'))
+        except ValueError as e:
+            messagebox.showerror(title='Invalid Network', message=f'Something went wrong. Check IP and Subnetmask. Error: {e}')
+
+
+    def validate_ip(self):
+        ip_lst = self.entry_ip.get_values()
+        ip = f"{ip_lst[0]}.{ip_lst[1]}.{ip_lst[2]}.{ip_lst[3]}"
+        try:
+            net.ipaddress.ip_address(ip)
+            return ip
+        except:
+            messagebox.showwarning(title='Invalid IP', message='Invalid IP-Address. Please enter a valid IP')
+            for octet in self.entry_ip.octet_list:
+                octet.delete(0, tk.END)
+                octet.insert(0,'0')
+
+    def get_mask(self):
+        if not self.entry_ip.cidr_field.instate(['readonly']):
+            cidr_value = self.entry_ip.get_cidr_value()
+            subnetmask = net.cidr_to_sub(cidr_value)
+        else:
+            sub_lst = self.entry_sub.get_values()
+            subnetmask = f'{sub_lst[0]}.{sub_lst[1]}.{sub_lst[2]}.{sub_lst[3]}'
+            if subnetmask not in net.cidr_to_subnet.values():
+                messagebox.showwarning(title='Invalid Mask', message='Invalid Subnetmask. Please enter a valid mask')
+                subnetmask = '0.0.0.0'
+                for octet in self.entry_sub.octet_list:
+                    octet.delete(0, tk.END)
+                    octet.insert(0,'0')
+        return subnetmask
+
+    def update_subnetmask(self, subnetmask):
         self.entry_sub.toggle_state('normal')
         counter = 0
         for octet in self.entry_sub.octet_list:
@@ -141,6 +185,9 @@ class FrameResult(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
+        image_url = './icons/copy_alt.png'
+        self.image = tk.PhotoImage(file=image_url)
+
         self.rowconfigure((1,2,3,4,5), weight=1, uniform='a')
         self.columnconfigure(1, weight=1, uniform='a')
         self.columnconfigure(2, weight=4, uniform='a')
@@ -164,11 +211,55 @@ class FrameResult(ttk.Frame):
         self.Label_broadcast_result = ttk.Label(self, text='0.0.0.0', font=('Arial', 10))
         self.Label_max_hosts_result = ttk.Label(self, text='0', font=('Arial', 10))
 
-        self.Label_netid_result.grid(row=0, column=1, columnspan=4, sticky='w')
-        self.Label_first_host_result.grid(row=1, column=1, columnspan=4, sticky='w')
-        self.Label_last_host_result.grid(row=2, column=1, columnspan=4, sticky='w')
-        self.Label_broadcast_result.grid(row=3, column=1, columnspan=4, sticky='w')
-        self.Label_max_hosts_result.grid(row=4, column=1, columnspan=4, sticky='w')
+        self.Label_netid_result.grid(row=0, column=1, columnspan=3, sticky='w')
+        self.Label_first_host_result.grid(row=1, column=1, columnspan=3, sticky='w')
+        self.Label_last_host_result.grid(row=2, column=1, columnspan=3, sticky='w')
+        self.Label_broadcast_result.grid(row=3, column=1, columnspan=3, sticky='w')
+        self.Label_max_hosts_result.grid(row=4, column=1, columnspan=3, sticky='w')
+
+        self.button_netid = tk.Button(
+            self, 
+            text='Copy', 
+            image=self.image, 
+            command=lambda: self.copy_to_clipboard(self.Label_netid_result.cget('text')))
+        self.button_first_host = tk.Button(
+            self, 
+            text='Copy', 
+            image=self.image, 
+            command=lambda: self.copy_to_clipboard(self.Label_first_host_result.cget('text')))
+        self.button_last_host = tk.Button(
+            self, 
+            text='Copy', 
+            image=self.image, 
+            command=lambda: self.copy_to_clipboard(self.Label_last_host_result.cget('text')))
+        self.button_broadcast = tk.Button(
+            self, 
+            text='Copy', 
+            image=self.image, 
+            command=lambda: self.copy_to_clipboard(self.Label_broadcast_result.cget('text')))
+        self.button_max_hosts = tk.Button(
+            self, 
+            text='Copy', 
+            image=self.image, 
+            command=lambda: self.copy_to_clipboard(self.Label_max_hosts_result.cget('text')))
+
+        self.button_netid.grid(row=0, column=4, sticky='e', ipady=0, ipadx=0)
+        self.button_first_host.grid(row=1, column=4, sticky='e')
+        self.button_last_host.grid(row=2, column=4, sticky='e')
+        self.button_broadcast.grid(row=3, column=4, sticky='e')
+        self.button_max_hosts.grid(row=4, column=4, sticky='e')
+
+    def update_labels(self, network_id, first_host, last_host, broadcast, max_hosts):
+        self.Label_netid_result.config(text=network_id)
+        self.Label_first_host_result.config(text=first_host)
+        self.Label_last_host_result.config(text=last_host)
+        self.Label_broadcast_result.config(text=broadcast)
+        self.Label_max_hosts_result.config(text=max_hosts)
+
+    def copy_to_clipboard(self, label):
+        self.clipboard_clear()
+        self.clipboard_append(label)
+        self.update()
 
 # run
 App()
